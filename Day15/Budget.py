d3fcf2ee64a13adf9fb8de2e3f3620f8427c7fe7
JaeBinLee <39715124+Albert-learner@users.noreply.github.com>
2020-12-20 16:10:35 +0900

    Add files via upload

--- Day15/Budget.py
+++ Day15/Budget.py
@@ -1,23 +1,37 @@
+# 37. 예산
+
 def solution(d, budget):
     answer = 0
+
+    # 총 가능 금액
+    total = 0
+
     d.sort()
-    
-    for dVal in d:
-        budget -= dVal
-        answer += 1
-        if budget < 0:
+    for i in d:
+        if total + i <= budget:
+            total += i
+            answer += 1
+        else:
             break
-    
-    if budget >= 0:
-        return answer
-    else:
-        return answer -1
+    return answer
 
-d1 = [1,3,2,5,4]
-d2 = [2,2,3,3]
+d_1 = [1, 3, 2, 5, 4]
+d_2 = [2, 2, 3, 3]
 
-budget1 = 9
-budget2 = 10
+budget_1 = 9
+budget_2 = 10
 
-print(solution(d1, budget1))
-print(solution(d2, budget2))
+print(solution(d_1, budget_1))
+print(solution(d_2, budget_2))
+
+def solution_best(d, budget):
+    answer = 0
+
+    d.sort()
+    while budget < sum(d):
+        d.pop()
+    answer = len(d)
+    return answer
+
+print(solution_best(d_1, budget_1))
+print(solution_best(d_2, budget_2))
