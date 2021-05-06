f4855644e88c715b19bd8ac958bf0afe72606afa
Robert.Yang <ysb05222@naver.com>
2021-05-06 22:35:19 +0900

    Update pocketmon.py

--- LEVEL2/Day1/pocketmon.py
+++ LEVEL2/Day1/pocketmon.py
@@ -12,7 +12,7 @@ def solution(nums):
     pocketmon = [] # 새로운 종류의 폰켓몬을 선택하면 이 리스트에 넣음.
     for i in nums:
         try:
-            if pocketmon.index(i) >= 0: # 겹치면 answer_list에 추가하지 않기
+            if pocketmon.index(i) >= 0: # 겹치면 list에 추가하지 않기
                 continue
         except:
             pocketmon.append(i)
